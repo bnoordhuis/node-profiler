c2e6576cc72d9a2d3c8dc1979ac9429da1afce3e
Ben Noordhuis <info@bnoordhuis.nl>
2012-01-20 17:05:43 +0100

    Add console.profile() and console.profileEnd()
    Mimics browser profiling API. Said methods are monkey-patched into the console
    object.

--- profiler.cc
+++ profiler.cc
@@ -23,9 +23,27 @@ Handle<Value> Pause(const Arguments& args) {
 
 extern "C" void init(Handle<Object> target) {
   HandleScope scope;
-  target->Set(String::New("gc"), FunctionTemplate::New(GC)->GetFunction());
-  target->Set(String::New("pause"), FunctionTemplate::New(Pause)->GetFunction());
-  target->Set(String::New("resume"), FunctionTemplate::New(Resume)->GetFunction());
+
+  target->Set(String::New("gc"),
+              FunctionTemplate::New(GC)->GetFunction());
+
+  target->Set(String::New("pause"),
+              FunctionTemplate::New(Pause)->GetFunction());
+
+  target->Set(String::New("resume"),
+              FunctionTemplate::New(Resume)->GetFunction());
+
+  // mimic browser API
+  Local<Object> global = Context::GetCurrent()->Global();
+  Local<Value> console_v = global->Get(String::New("console"));
+  if (console_v.IsEmpty() || !console_v->IsObject()) return;
+  Local<Object> console = console_v->ToObject();
+
+  console->Set(String::New("profile"),
+               FunctionTemplate::New(Resume)->GetFunction());
+
+  console->Set(String::New("profileEnd"),
+               FunctionTemplate::New(Pause)->GetFunction());
 }
 
 }
